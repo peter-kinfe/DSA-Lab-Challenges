faaa689f6fa47d07b404cc5004d83993870d1690
peter <peterkinfe548@gmail.com>
2025-03-15 20:00:17 +0300

    Week1_Challenge_TwoSum

--- Week1_Challenge_TwoSum.cpp
+++ Week1_Challenge_TwoSum.cpp
@@ -1,4 +1,4 @@
-// brute force solution this is the first approach
+// brute force solution this is the first approach, the time complexity is O(n^2) and the space complexity is O(1)
 
 // vector<int> twoSum(vector<int>& nums, int target) { //function to find the indices of the two numbers that add up to the target
 //     for (int i = 0; i < nums.size(); i++) {       //outer loop
@@ -8,11 +8,11 @@
 //             }
 //         }
 //     }
-//     return {};// return empty vector if no solution is found
+//     return {};// return empty vector if there is no solution
 // }
 
 
-// this is the second approach which is the hash table approach
+// this is the second approach which is the hash table approach which is more efficient than the brute force one and the time and space complexity is O(n)
 #include <vector>
 #include <unordered_map>
 
@@ -21,7 +21,6 @@ class Solution {
     public:
         vector<int> twoSum(vector<int>& nums, int target) {
             unordered_map<int, int> map;  // it is a hash table that stores key-value pairs, which is used for mapping the numbers to their index
-             //this is the second approach which is the hash table approach
              for (int i = 0; i < nums.size(); i++) {
                 int current = nums[i]; // holds the value of the current number
                 //target = x(some unknown value to be added to our current value to found the target) + cur(current value)
