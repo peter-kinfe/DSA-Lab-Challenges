6861f2e102554539067d958a188063b5c49137ec
peter <peterkinfe548@gmail.com>
2025-03-15 19:18:05 +0300

    Week1_Challenge_TwoSum

--- Week1_Challenge_TwoSum.cpp
+++ Week1_Challenge_TwoSum.cpp
@@ -1,3 +1,15 @@
+// vector<int> twoSum(vector<int>& nums, int target) { //function to find the indices of the two numbers that add up to the target
+//     // Brute force solution this is the first approach
+//     for (int i = 0; i < nums.size(); i++) {       //outer loop
+//         for (int j = i + 1; j < nums.size(); j++) { //inner loop
+//             if (nums[i] + nums[j] == target) {
+//                 return {i, j};  //return indices
+//             }
+//         }
+//     }
+//     return {};// return empty vector if no solution is found
+// }
+
 #include <vector>
 #include <unordered_map>
 
@@ -7,7 +19,7 @@ class Solution {
     public:
         vector<int> twoSum(vector<int>& nums, int target) {
             unordered_map<int, int> map;  // it is a hash table that stores key-value pairs, which is used for mapping the numbers to their index
-    
+             //this is the second approach which is the hash table approach
              for (int i = 0; i < nums.size(); i++) {
                 int current = nums[i]; // holds the value of the current number
                 //target = x(some unknown value to be added to our current value to found the target) + cur(current value)
@@ -20,6 +32,6 @@ class Solution {
     
                 map[current] = i; //store the current value and its index in the map
             }
-            return {};
+            return {}; //return empty if there is no solution
         }
     };
